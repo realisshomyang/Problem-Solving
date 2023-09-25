9adfe3a8aa47c4e507d03f9dffd5bb3bbdaee18d
MyeongGeun CHO <81310047+realisshomyang@users.noreply.github.com>
2023-09-25 12:38:33 +0000

    [Silver III] Title: 계단 오르기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,11 @@
+n=int(input()) # 계단 개수
+s=[int(input()) for _ in range(n)] # 계단 리스트
+dp=[0]*(n)
+if len(s)<=2 : 
+    print(sum(s))
+else: 
+    dp[0]=s[0] 
+    dp[1]=s[0]+s[1] 
+    for i in range(2,n): 
+        dp[i]=max(dp[i-3]+s[i-1]+s[i], dp[i-2]+s[i])
+    print(dp[-1])
