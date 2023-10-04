d0a5482e5173d2247f6580c0a8267fe72ef99705
MyeongGeun CHO <81310047+realisshomyang@users.noreply.github.com>
2023-10-04 14:50:00 +0000

    [Silver III] Title: 파도반 수열, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,13 @@
+dp = [0]*101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+
+for i in range(4, 101):
+    dp[i] = dp[i-2] + dp[i-3]
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    print(dp[n])
